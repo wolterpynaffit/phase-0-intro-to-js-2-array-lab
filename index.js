1b3873e0433f95ff99253cfde13aaa1ec557ef40
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2023-05-02 17:07:50 -0400

    completed array 2 lab

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Write your solution here!
+const cats = [
+    "Milo", 
+    "Otis", 
+    "Garfield"
+]
+
+function destructivelyAppendCat(name) {
+    let addLast = cats.push(name)
+    return addLast
+}
+
+function destructivelyPrependCat(name){
+    let addFront = cats.unshift(name)
+    return addFront
+}
+
+function destructivelyRemoveLastCat(name){
+    let removeLast = cats.pop()
+    return removeLast
+}
+
+function destructivelyRemoveFirstCat(name){
+    let removeFront = cats.shift()
+    return removeFront
+}
+
+function appendCat(name){
+    let copyCat = [...cats, name]
+    return copyCat
+    
+  }
+
+
+  function prependCat(name){
+    let copyCat = [name,...cats]
+    return copyCat
+    
+  }
+
+
+  function removeLastCat(){
+    const copyCat = [...cats]
+    let removeLast = copyCat.pop()
+    return copyCat
+
+  }
+
+  function removeFirstCat(){
+    const copyCat = [...cats]
+    let removeFirst = copyCat.shift()
+    return copyCat
+  }
+
